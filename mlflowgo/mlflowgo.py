a51d503c417d19acc5067bde7c61bd28f55ce33e
James Weaver <jceweaver@googlemail.com>
2023-11-30 07:08:52 +0000

    major refactor

--- mlflowgo/mlflowgo.py
+++ mlflowgo/mlflowgo.py
@@ -1,18 +1,20 @@
 from .artifact_logger import ArtifactLogger
+from . import CLASSIFIER_KEY
+from .base import Base
 import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.base import is_classifier
-from sklearn.metrics._scorer import _SCORERS
 import subprocess
 import threading
 import webbrowser
+import requests
 import time
 import pandas as pd
 
 
-class MLFlowGo:
+class MLFlowGo(Base):
     """
     A class to integrate MLFlow with scikit-learn pipelines for data scientists.
 
@@ -37,70 +39,83 @@ class MLFlowGo:
             mlflow.set_tracking_uri(tracking_uri)
         mlflow.set_experiment(experiment_name)
 
-    def run_experiment(self, pipeline: Pipeline, X: pd.DataFrame, y: pd.DataFrame, cv: int = 5, **kwargs):
+    def run_experiment(self, X: pd.DataFrame, y: pd.DataFrame, cv: int = 5, **kwargs):
         """
         Runs a cross-validation experiment with the given pipeline and data, logs the metrics and model in MLFlow.
 
         Parameters:
             pipeline (sklearn.pipeline.Pipeline): The scikit-learn compatible pipeline to evaluate.
-            X (array-like): Feature dataset.
-            y (array-like): Target values.
+            X (pd.DataFrame): Feature dataset.
+            y (pd.DataFrame): Target values.
             cv (int, optional): Number of cross-validation splits. Defaults to 5.
             metrics (list of str, optional): Metrics to log. Defaults to ['accuracy'].
             **kwargs: Additional keyword arguments for cross_val_score function.
         """
-        task_type = kwargs.get('task_type', None)
-        metrics = kwargs.get('metrics', None)
-        feature_names = kwargs.get('feature_names', X.columns)
+        self.pipeline, self.task_type = self.get_pipeline(
+            kwargs.get('pipeline', None),
+            kwargs.get('task_type', None))
+        self.metrics = self.get_model_metrics(
+            kwargs.get('metrics', None),
+            self.task_type)
+        self.feature_names = self.get_feature_names(
+            kwargs.get('feature_names', None),
+            X.columns
+        )
+        self.model_step = self.get_model_step_from_pipeline(self.pipeline)
 
-        if task_type is None:
-            task_type = 'classification' if is_classifier(pipeline) else 'regression'
-
-        if metrics is None:
-            metrics = self._get_default_metrics(task_type)
-
-        with mlflow.start_run(run_name=self._get_run_name(pipeline)):
+        with mlflow.start_run(run_name=self.get_run_name(self.pipeline)):
             # Perform cross-validation
-            cv_results = cross_val_score(pipeline, X, y, cv=cv, scoring=metrics[0], **kwargs)
+            cv_results = cross_val_score(
+                self.pipeline, X, y, cv=cv, scoring=self.metrics[0])
 
             # Log parameters, metrics, and model
-            self._log_params(pipeline)
-            self._log_metrics(cv_results, metrics)
-            if task_type == 'classification':
-                self._log_artifacts(pipeline, X, y, feature_names)
-            mlflow.sklearn.log_model(pipeline,
-                                     self._get_model_step_from_pipeline(pipeline))
+            self._log_params(self.pipeline)
+            self._log_metrics(cv_results, self.metrics)
+            if self.task_type == CLASSIFIER_KEY:
+                self._log_artifacts(self.pipeline, X, y, self.feature_names)
+            mlflow.sklearn.log_model(self.pipeline,
+                                     self.model_step)
 
     def _log_artifacts(self, pipeline, X, y, feature_names):
         """
-        Log artifacts
+        Log all relevant artifacts for the experiment
+
+        Parameters:
+            pipeline (sklearn.pipeline.Pipeline): The scikit-learn compatible pipeline to evaluate.
+            X (pd.DataFrame): Feature dataset.
+            y (pd.DataFrame): Target values.
+            feature_names (array-like): Typically the column names of X
         """
         artifact_logger = ArtifactLogger()
         pipeline.fit(X, y)
         y_pred, y_scores = pipeline.predict(X), pipeline.predict_proba(X)
 
-        # Plot and log ROC curve
-        artifact_logger.plot_roc_curve(y,
-                                       y_scores,
-                                       feature_names)
-
-        # Plot and log confusion matrix
-        artifact_logger.plot_confusion_matrix(y,
-                                              y_pred)
+        if is_classifier(pipeline):
+            # Plot and log ROC curve
+            artifact_logger.plot_roc_curve(y,
+                                           y_scores,
+                                           feature_names)
+            # Plot and log confusion matrix
+            artifact_logger.plot_confusion_matrix(y,
+                                                  y_pred)
 
         # Save and log data sample
         artifact_logger.save_data_sample(X,
                                          100)  # Log 100 samples
 
         # Plot and log feature importance
-        model_step = self._get_model_step_from_pipeline(pipeline)
-        if hasattr(pipeline.named_steps[model_step], 'feature_importances_'):
+        if hasattr(pipeline.named_steps[self.model_step], 'feature_importances_'):
             artifact_logger.plot_feature_importance(pipeline,
-                                                    model_step,
+                                                    self.model_step,
                                                     feature_names)
 
     def _log_params(self, pipeline):
-        """Logs the parameters of the model or pipeline."""
+        """
+        Logs the parameters of the model or pipeline.
+
+        Parameters:
+            pipeline (sklearn.pipeline.Pipeline): The scikit-learn compatible pipeline to evaluate.
+        """
         if isinstance(pipeline, Pipeline):
             params = pipeline.get_params()
         else:
@@ -113,65 +128,28 @@ class MLFlowGo:
             mlflow.log_metric(f"mean_{metric}", cv_results.mean())
             mlflow.log_metric(f"std_{metric}", cv_results.std())
 
-    def _get_run_name(self, pipeline):
-        """
-        Generates a run name based on the model or pipeline.
-
-        Parameters:
-            pipeline (sklearn.pipeline.Pipeline): The pipeline for which to generate the run name.
-
-        Returns:
-            str: Generated run name.
-        """
-        if isinstance(pipeline, Pipeline):
-            name = "|".join([step[0] for step in pipeline.steps])
-        else:
-            name = type(pipeline).__name__
-        return name
-
-    def _get_model_step_from_pipeline(self, pipeline):
-        """
-        Identify the model step from the pipeline
-        Parameters:
-            pipeline (sklearn.pipeline.Pipeline): The pipeline object
-        returns:
-            step_name (str): Name of the step
-        """
-        if not isinstance(pipeline, Pipeline):
-            raise ValueError("The provided object is not a scikit-learn Pipeline.")
-
-        for step_name, step in pipeline.steps:
-            # A model is expected to have a 'predict' method
-            if hasattr(step, 'predict'):
-                return step_name
-
-    def _get_default_metrics(self, task_type):
-        """Returns a list of default metrics based on the task type."""
-        metrics = []
-        for scorer_name, scorer in _SCORERS.items():
-            if task_type == 'classification' and scorer._sign == 1:  # Classification metrics
-                metrics.append(scorer_name)
-            elif task_type == 'regression' and scorer._sign == -1:  # Regression metrics
-                metrics.append(scorer_name)
-        return metrics
-
     def run_mlflow_ui(self, port=5000, open_browser=True):
         """
         Starts the MLflow UI server on a specified port and optionally opens it in a web browser.
-
-        Parameters:
-            port (int, optional): The port to run the MLflow UI on. Defaults to 5000.
-            open_browser (bool, optional): If True, opens the MLflow UI in the default web browser. Defaults to True.
         """
-        def start_server():
-            subprocess.run(["mlflow", "ui", "--port", str(port)])
 
-        # Start the MLflow UI in a separate thread
-        thread = threading.Thread(target=start_server)
-        thread.daemon = True
-        thread.start()
+        def is_server_running():
+            """ Check if the MLflow UI server is already running """
+            try:
+                requests.get(f"http://localhost:{port}")
+                return True
+            except requests.ConnectionError:
+                return False
 
-        if open_browser:
-            # Give the server a moment to start before opening the browser
-            time.sleep(2)  
-            webbrowser.open(f"http://localhost:{port}")
+        if not is_server_running():
+            # Start the server if not already running
+            def start_server():
+                subprocess.run(["mlflow", "ui", "--port", str(port)])
+
+            thread = threading.Thread(target=start_server)
+            thread.daemon = True
+            thread.start()
+
+            if open_browser:
+                time.sleep(2)  # Give the server a moment to start
+                webbrowser.open(f"http://localhost:{port}")
