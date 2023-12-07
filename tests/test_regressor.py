78a94aac01b3641f4e43c8f6e6cb9361399aa385
James Weaver <jceweaver@googlemail.com>
2023-12-07 08:00:47 +0000

    write test for lasso regression

--- tests/test_regressor.py
+++ tests/test_regressor.py
@@ -8,6 +8,21 @@ import numpy as np
 import pandas as pd
 
 
+def test_lasso_pipeline():
+    """ Test MLFlowGo with a pipeline containing AdaBoostClassifier """
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
+    df = pd.read_csv(url, delimiter=';')
+
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('model', Lasso())
+    ])
+    mlflow_go = MLFlowGo(experiment_name="lasso_regression_test")
+    mlflow_go.run_experiment(pipeline=pipeline,
+                             X=df.drop('quality', axis=1),
+                             y=df['quality'], cv=-1)
+
+
 def test_linear_regression_pipeline():
     """ Test MLFlowGo with a pipeline containing AdaBoostClassifier """
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
