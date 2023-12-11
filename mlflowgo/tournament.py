1d43934d92646f7b593bec57d5d266282e4c992b
James Weaver <jceweaver@googlemail.com>
2023-12-11 08:42:49 +0000

    handle cases when the pipeline is not available in the known params dict

--- mlflowgo/tournament.py
+++ mlflowgo/tournament.py
@@ -1,5 +1,6 @@
 from .base import Base
 from sklearn.model_selection import RandomizedSearchCV, cross_val_score
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
 
@@ -8,10 +9,10 @@ class Tournament(Base):
     A class to run a tournament style selection to finding the best model, logging to MLflow
     """
 
-    def __init__(self, X, y, pipelines=None):
+    def __init__(self, X_train, y_train, X_test, y_test, pipelines=None):
 
-        self.X = X
-        self.y = y
+        self.X_train = X_train
+        self.y_train = y_train
         self.pipelines = pipelines
         self.models = {}
         self.models_params = {}
@@ -27,10 +28,11 @@ class Tournament(Base):
             model_step = self.get_model_step_from_pipeline(pipeline)
             model_name = pipeline.named_steps[model_step].__class__.__name__
             model_param = self.get_param_dist(model_name)
+
+            self.models[model_name] = pipeline
             if model_param is not None:
-                self.models[model_name] = pipeline
-                model_param = {f'{model_step}__{i}':j for i, j in model_param.items()}
-                self.models_params[model_name] = model_param
+                model_param = {f'{model_step}__{i}': j for i, j in model_param.items()}
+            self.models_params[model_name] = model_param
 
     def _run_cv_param_search(self):
         """
@@ -40,24 +42,38 @@ class Tournament(Base):
         final_scores = {}
 
         for model_name in self.models:
-            search = RandomizedSearchCV(
-                self.models[model_name],
-                param_distributions=self.models_params[model_name],
-                n_iter=n_iter_search,
-                cv=5,
-                scoring='neg_mean_squared_error',
-                verbose=3
-            )
-            search.fit(self.X, self.y)
-            best_model = search.best_estimator_
-            cv_scores = cross_val_score(
-                best_model,
-                self.X,
-                self.y,
-                cv=5,
-                scoring='neg_mean_squared_error',
-                verbose=3
-            )
-            final_scores[model_name] = np.mean(cv_scores)
-            best_model_name = min(final_scores, key=final_scores.get)
+            if self.models_params[model_name] is not None:
+                search = RandomizedSearchCV(
+                    self.models[model_name],
+                    param_distributions=self.models_params[model_name],
+                    n_iter=n_iter_search,
+                    cv=5,
+                    scoring='neg_mean_squared_error',
+                    verbose=3
+                )
+                search.fit(self.X_train, self.y_train)
+                best_model = search.best_estimator_
+                cv_scores = cross_val_score(
+                    best_model,
+                    self.X_train,
+                    self.y_train,
+                    cv=5,
+                    scoring='neg_mean_squared_error',
+                    verbose=3
+                )
+                final_scores[model_name] = np.mean(cv_scores)
+            else:
+                model = self.models[model_name]
+                model.fit(self.X_train, self.y_train)
+                cv_scores = cross_val_score(
+                    model,
+                    self.X_train,
+                    self.y_train,
+                    cv=5,
+                    scoring='neg_mean_squared_error',
+                    verbose=3
+                )
+                final_scores[model_name] = np.mean(cv_scores)
+
+        best_model_name = min(final_scores, key=final_scores.get)
         print(f'best model is {best_model_name}')
