3cb57bb0a55db933d3e1ab824c3aff2ebb82d2b7
James Weaver <jceweaver@googlemail.com>
2023-12-13 17:41:16 +0000

    remove attributes from class

--- mlflowgo/artifact_base.py
+++ mlflowgo/artifact_base.py
@@ -9,24 +9,13 @@ from sklearn.linear_model import (
     RidgeClassifier, SGDClassifier, Perceptron)
 from sklearn.pipeline import Pipeline
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor, ExtraTreeRegressor, ExtraTreeClassifier
 
 
 class ArtifactBase():
 
-    def __init__(self, **kwargs) -> None:
-        self.pipeline = kwargs.get('pipeline', None)
-        self.X_train = kwargs.get('X_train', None)
-        self.y_train = kwargs.get('y_train', None)
-        self.X_test = kwargs.get('X_test', None)
-        self.y_test = kwargs.get('y_test', None)
-        self.model_step = kwargs.get('model_step', None)
-        self.feature_names = kwargs.get('feature_names', None)
-        self.metric = kwargs.get('metric', None)
-        self.param_name = kwargs.get('param_name', None)
-        self.param_range = kwargs.get('param_range', None)
-        self.objective = kwargs.get('objective', None)
-        self.dataset_desc = kwargs.get('dataset_desc', None)
+    def __init__(self) -> None:
+        pass
 
     @classmethod
     def get_shap_explainer(self, pipeline, model_step, X):
@@ -56,7 +45,8 @@ class ArtifactBase():
                       (RandomForestClassifier, GradientBoostingClassifier,
                        DecisionTreeClassifier, DecisionTreeRegressor,
                        ExtraTreesRegressor, RandomForestRegressor,
-                       GradientBoostingRegressor, ExtraTreesClassifier)):
+                       GradientBoostingRegressor, ExtraTreesClassifier,
+                       ExtraTreeRegressor, ExtraTreeClassifier)):
             return shap.TreeExplainer(model), X
 
         # Linear models
@@ -72,6 +62,8 @@ class ArtifactBase():
         else:
             # Default to Explainer for models not explicitly handled above
             if hasattr(model, 'predict_proba'):
-                return shap.KernelExplainer(model.predict_proba, X), X
+                return shap.KernelExplainer(model.predict_proba,
+                                            X), X
             else:
-                return shap.KernelExplainer(model.predict, X), X
+                return shap.KernelExplainer(model.predict,
+                                            X), X
