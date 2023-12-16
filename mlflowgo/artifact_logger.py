d363e985874d9173b5904051ba01499db18b33e4
James Weaver <jceweaver@googlemail.com>
2023-12-16 16:37:27 +0000

    update doc string for log_qq_plot

--- mlflowgo/artifact_logger.py
+++ mlflowgo/artifact_logger.py
@@ -1160,11 +1160,52 @@ class ArtifactLogger:
 
     def log_qq_plot(self, pipeline, X_test, y_test):
         """
-        Generates and logs a Q-Q plot as an MLflow artifact to assess normality of residuals.
+        Generates and logs a Quantile-Quantile (Q-Q) plot as an MLflow artifact to assess the normality of residuals.
+
+        A Q-Q plot is a graphical tool used to determine whether a dataset follows a specific theoretical distribution, 
+        such as a normal distribution. In the context of regression analysis, it is often used to assess the normality of 
+        residuals (the differences between observed and predicted values).
 
         Parameters:
-        pipeline (sklearn.pipeline.Pipeline): Object type that implements the "fit" and "predict" methods
-        X_test, y_test (pd.DataFrame): Test dataset (features and target).
+        pipeline (sklearn.pipeline.Pipeline): An object implementing the "fit" and "predict" methods.
+
+        X_test, y_test (pd.DataFrame): Test dataset, consisting of features (X_test) and target values (y_test).
+
+        Example:
+        ```python
+        from sklearn.pipeline import Pipeline
+        from sklearn.linear_model import LinearRegression
+        from sklearn.model_selection import train_test_split
+        import pandas as pd
+
+        # Create a pipeline with a linear regression model
+        model = Pipeline([
+            ('regressor', LinearRegression())
+        ])
+
+        # Generate example data
+        X = pd.DataFrame({'Feature1': [1, 2, 3, 4, 5], 'Feature2': [2, 4, 5, 4, 5]})
+        y = pd.Series([3, 5, 6, 7, 8])
+
+        # Split the data into training and test sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Fit the model and log the Q-Q plot
+        model.fit(X_train, y_train)
+        your_artifact_base.log_qq_plot(model, X_test, y_test)
+        ```
+
+        Notes:
+        - The Q-Q plot is generated to assess the normality of residuals in regression analysis.
+        - Residuals are calculated as the differences between observed (y_test) and predicted values.
+        - The Q-Q plot is saved as a temporary image file and logged as an MLflow artifact with the label 'Metrics'.
+        - The temporary file is removed after logging.
+
+        Raises:
+        - None
+
+        Returns:
+        - None
         """
         residuals = y_test - pipeline.predict(X_test)
         plt.figure(figsize=(8, 6))
